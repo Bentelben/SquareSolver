e6ec517d39ddff48c47e970ed1e82caf202a6651
Bentelben <mr.bentelben@gmail.com>
2025-08-14 09:00:39 +0300

    Refactor: separate reading and writing from main

--- main.cpp
+++ main.cpp
@@ -2,21 +2,29 @@
 
 #include "square_solver.hpp"
 
+int readIn(double *a, double *b, double *c);
+void writeOut(RootCount root_count, double x1, double x2);
+
 int main() {
-    double a = 0, b = 0, c = 0;
-    
+    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+    if (readIn(&a, &b, &c) != 0) return -1;
+    printf("Solving...\n");
+    const RootCount root_count = solveSquareEquation(a, b, c, &x1, &x2);
+    writeOut(root_count, x1, x2);
+}
+
+int readIn(double *const a, double *const b, double *const c) {
     printf("ax^2 + bx + c = 0\n");
     printf("Enter a, b and c:\n");
-    if (scanf("%lf %lf %lf", &a, &b, &c) != 3) {
+    if (scanf("%lf %lf %lf", a, b, c) != 3) {
         printf("Wrong input\n");
         return -1;
     }
-    printf("Solving...\n");
+    return 0;
+}
 
-    double x1 = 0, x2 = 0;
-    const RootCount count = solveSquareEquation(a, b, c, &x1, &x2);
-
-    switch (count) {
+void writeOut(const RootCount root_count, const double x1, const double x2) {
+    switch (root_count) {
         case INF:
             printf("x is any real number\n");
             break;
@@ -33,5 +41,4 @@ int main() {
             printf("Error\n");
             break;
     }
-    return 0;
 }
