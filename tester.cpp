51ba53cc0abdad5e2873df1e565f33818a6c1119
Bentelben <mr.bentelben@gmail.com>
2025-08-23 19:49:54 +0300

    Refactor: separate one unit test to new function

--- tester.cpp
+++ tester.cpp
@@ -12,6 +12,17 @@ struct Test_squareSolver {
     double x1, x2;
 };
 
+static void unitTestSquareSolver(double a, double b, double c, double answer_nRoots, double asnwer_x1, double answer_x2) {
+    double x1 = 0, x2 = 0;
+    RootCount nRoots = solveSquareEquation(tests[i].a, tests[i].b, tests[i].c, &x1, &x2);
+    if ( !(nRoots == tests[i].nRoots && isEqual(tests[i].x1, x1) && isEqual(tests[i].x2, x2)) ) {
+        printf("Wrong answer!\n");
+        printf("a = %g b = %g c = %g\n", tests[i].a, tests[i].b, tests[i].c);
+        printf("got answer nRoots = %d x1 = %g x2 = %g\n", nRoots, x1, x2);
+        printf("should be  nRoots = %d x1 = %g x2 = %g\n\n", tests[i].nRoots, tests[i].x1, tests[i].x2);
+    }
+}
+
 int testSquareSolver() {
     printf("Testing...\n");
 
@@ -23,21 +34,11 @@ int testSquareSolver() {
         {-1,  0,  0, ONE,  0,  0},
         { 0,  1,  0, ONE,  0,  0},
         { 0, -1,  0, ONE,  0,  0},
-        {NAN, NAN, NAN, INF, NAN, NAN}
+        { 0,  0,  1, ZERO, 0,  0}
     };
 
-
-    for (int i = 0; !isnan(tests[i].a); i++) {
-        double x1 = 0, x2 = 0;
-        RootCount nRoots = solveSquareEquation(tests[i].a, tests[i].b, tests[i].c, &x1, &x2);
-        if ( !(nRoots == tests[i].nRoots && isEqual(tests[i].x1, x1) && isEqual(tests[i].x2, x2)) ) {
-            printf("Wrong answer!\n");
-            printf("a = %g b = %g c = %g\n", tests[i].a, tests[i].b, tests[i].c);
-            printf("got answer nRoots = %d x1 = %g x2 = %g\n", nRoots, x1, x2);
-            printf("should be  nRoots = %d x1 = %g x2 = %g\n\n", tests[i].nRoots, tests[i].x1, tests[i].x2);
-            return -1;
-        }
-    }
+    for (int i = 0; i < sizeof(tests); i++)
+        unitTestSquareSolver(tests[i].a, tests[i].b, tests[i].c, tests[i].nRoots, tests[i].x1, tests[i].x2); 
 
     printf("Done testing\n\n");
     return 0;
