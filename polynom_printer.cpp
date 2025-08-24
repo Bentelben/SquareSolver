c58e05a4f871e95f1a73b70b55d77057455c45f3
Bentelben <mr.bentelben@gmail.com>
2025-08-24 10:58:25 +0300

    Fix: handle -1 coefficient in printing polynom

--- polynom_printer.cpp
+++ polynom_printer.cpp
@@ -6,10 +6,6 @@
 #include <math.h>
 #include <assert.h>
 
-static void PrintSpacedSignedValue(const double value) {
-    printf("%c %lg", (value < 0) ? '-' : '+', value);
-}
-
 char GetCoefficientName(const size_t index) {
     assert('a' + (char)index <= 'z');
     return 'a' + (char)index;
@@ -23,18 +19,25 @@ void PrintLetterPolynom(const size_t polynomPower) {
 }
 
 void PrintPolynom(const double *const coefficients, const size_t nCoefficients) {
-    bool isNotZero = false;
+    bool isFirst = true;
     for (size_t i = 0; i < nCoefficients; i++) {
         if (IsZero(coefficients[i])) continue;
-        isNotZero = true;
-        if (!IsEqual(coefficients[i], 1)) {
-            if (i == 0) printf("%lg", coefficients[i]);
-            else PrintSpacedSignedValue(coefficients[i]);
-        }
+
+        double absValue = fabs(coefficients[i]);
+        bool isPositive = coefficients[i] > 0;
+
+        if (!isPositive) printf("-");
+        else if (!isFirst) printf("+");
+        
+        if (!isFirst) printf(" ");
+
+        if (i == nCoefficients-1 || !IsEqual(absValue, 1)) printf("%lg", absValue);
+
         if (i < nCoefficients-1) printf("x");
         if (i < nCoefficients-2) printf("^%lu", nCoefficients-1-i);
         printf(" ");
+        isFirst = false;
     }
-    if (!isNotZero) printf("0 ");
+    if (isFirst) printf("0 ");
     printf("= 0\n");
 }
