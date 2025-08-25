25502f9a545fcf09542b6900399d3c9a18843291
Bentelben <mr.bentelben@gmail.com>
2025-08-25 20:04:35 +0300

    Style: add docs for display controller and clean buffer

--- utils/buffer_cleaner.h
+++ utils/buffer_cleaner.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 
+//! Cleans read buffer until new line or EOF
+//!
+//! @param[in] stream Stream for clearing buffer
 void CleanBufferLine(FILE* stream);
 
 #endif
--- utils/display_controller.h
+++ utils/display_controller.h
@@ -22,7 +22,15 @@ enum ColorType {
     BACKGROUND = 10
 };
 
+
+//! Changes text stdout color
+//!
+//! @param[in] color      Color of text
+//! @param[in] brightness Brightness of color
+//! @param[in] type       Foregound/background
 void SetColor(Color color, ColorBrightness brightness, ColorType type);
+
+//! Resets all text attributes (include color) in stdout
 void ResetTextAttributes();
 
 #endif
