38d0eb3232a3c7b9404718c241104188732d543c
Bentelben <mr.bentelben@gmail.com>
2025-08-25 15:01:05 +0300

    Refactor: init var and add assert in buffer cleaner

--- utils/buffer_cleaner.cpp
+++ utils/buffer_cleaner.cpp
@@ -1,9 +1,12 @@
 #include "buffer_cleaner.h"
 
 #include <stdio.h>
+#include <assert.h>
 
 void CleanBufferLine(FILE* stream) {
-    int c;
+    assert(stream);
+
+    int c = 0;
     do {
         c = getc(stream);
     } while ( c != EOF && c != '\n');
