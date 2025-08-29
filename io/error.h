2d24503ef528fbbd9f1cb256c2f0adb60abfd8de
Bentelben <mr.bentelben@gmail.com>
2025-08-29 12:38:22 +0300

    Style: add docs for PrintError

--- io/error.h
+++ io/error.h
@@ -1,6 +1,12 @@
 #ifndef ERROR_H
 #define ERROR_H
 
+//! Prints error message
+//!
+//! @param[in] format Format of message
+//! @param[in] ...    Arguments for format specification
+//!
+//! @note Usage similar to `printf(format, ...)`
 void PrintError(const char *format, ...);
 
 #endif
