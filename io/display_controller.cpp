ac90e4c22a979218c59b8aa2d29e3d79b1db584f
Bentelben <mr.bentelben@gmail.com>
2025-08-29 08:58:17 +0300

    Style: add assertions for stream pointers

--- io/display_controller.cpp
+++ io/display_controller.cpp
@@ -2,17 +2,19 @@
 
 #include <stdio.h>
 
+#include "../utils/myassert.h"
+
 static const char *const ESCAPE_STRING = "\033[";
 static const char *const SEPARATOR_STRING = ";";
 static const char *const CLOSING_STRING = "m";
 
 static void FPrintControlSequence(FILE *stream, const int n, const int m) {
+    myassert(stream, "Ptr to stream is NULL");
     fprintf(stream, "%s%d%s%d%s", ESCAPE_STRING, n, SEPARATOR_STRING, m, CLOSING_STRING);
 }
 
-
-
 void FSetColor(FILE *stream, const Color color, const ColorBrightness brightness, const ColorType type) {
+    myassert(stream, "Ptr to stream is NULL");
     const int code = (int)color + (int)brightness + (int)type;
     FPrintControlSequence(stream, 0, code);
 };
@@ -22,6 +24,7 @@ void SetColor(const Color color, const ColorBrightness brightness, const ColorTy
 }
 
 void FResetTextAttributes(FILE *stream) {
+    myassert(stream, "Ptr to stream is NULL");
     FPrintControlSequence(stream, 0, 0);
 }
 
